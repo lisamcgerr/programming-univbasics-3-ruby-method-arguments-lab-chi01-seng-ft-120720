21bef8e87efda55bc354ef2263ae2294853653d0
Lisa Marie McGerr <lisamarie.mcgerr@gmail.com>
2020-11-06 20:52:30 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,12 +5,17 @@ end
 introduction("Edwin")
 
 
-def introduction_with_language(name, language = "Ruby")
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language("Edwin")
+introduction_with_language("Edwin", "Python")
 
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language_optional("Edwin")
 
 
 
